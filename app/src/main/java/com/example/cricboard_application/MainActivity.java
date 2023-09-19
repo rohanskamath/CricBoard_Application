d2d8ef16af4b9a2fec21e3d41c3fc6e4b7cf3a34
GangliaTechno <136331251+GangliaTechno@users.noreply.github.com>
2023-09-19 14:19:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cricboard_application/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.cricboard_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+import com.example.cricboard_application.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+        /* Changing Action Bar color */
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#072B5A")));
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+        if(item.getItemId()==R.id.menuHome) {
+            replaceFragment(new HomeFragment());
+        }else if(item.getItemId()==R.id.menuTeams){
+            replaceFragment(new TeamFragment());
+        }
+        return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+    }
+}
